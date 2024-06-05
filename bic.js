89a948a873f197a8943d0a63f9699565ac0b462d
alamin-hossen-it <alamin.it24to7@.com>
2024-06-05 17:07:48 +0300

    Website for Baharain Company

---
+++ bic.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Have Most Exprienced Engineer." , "Trust On Our Employes." , "Trying To Give The Best Opurtunaty." , "Giveing 24/7 Service."] ,
+    typeSpeed:120,
+    backSpeed:120,
+    backDelay:1000,
+    loop:true
+});
